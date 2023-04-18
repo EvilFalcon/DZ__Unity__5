e46be0aca6855e51f4b66184a49a984bdf143797
EvilFalcon <112806793+EvilFalcon@users.noreply.github.com>
2023-04-18 23:12:05 +0300

    rew

--- Assets/scripts/SirenActivationScript.cs
+++ Assets/scripts/SirenActivationScript.cs
@@ -1,15 +1,29 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class SirenActivationScript : MonoBehaviour
 {
     [SerializeField] private float _stepVolume;
-    [SerializeField] private Volume _volume;
+    [SerializeField] private AudioSource _audioSource;
+    private Coroutine _coroutine;
+    private const float MaxVolume = 1;
+    private const float MinVolume = 0;
+    
+    private event Action OnPlay;
+    private event Action OfPlay;
+    
+    private void Awake()
+    {
+        OnPlay += Enable;
+        OfPlay += Disable;
+    }
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.TryGetComponent<Player>(out Player player))
         {
-            _volume.Enable(_stepVolume);
+            OnPlay?.Invoke();
         }
     }
 
@@ -17,7 +31,44 @@ public class SirenActivationScript : MonoBehaviour
     {
         if (col.TryGetComponent<Player>(out Player player))
         {
-            _volume.Disable(_stepVolume);
+            OfPlay?.Invoke();
+        }
+    }
+    
+    private void Enable()
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _audioSource.volume = 0;
+        _audioSource.Play();
+        Debug.Log(_audioSource.volume);
+
+        _coroutine = StartCoroutine(ChangeVolume(_stepVolume, MaxVolume));
+    }
+
+    private void Disable()
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeVolume(_stepVolume, MinVolume));
+
+        if (_audioSource.volume == 0)
+            _audioSource.Stop();
+    }
+
+    private IEnumerator ChangeVolume(float step, float targgetValue)
+    {
+        while (Math.Abs(_audioSource.volume - targgetValue) > Mathf.Epsilon)
+        {
+            _audioSource.volume = Mathf.MoveTowards(
+                _audioSource.volume,
+                targgetValue,
+                step * Time.deltaTime
+            );
+
+            yield return null;
         }
     }
 }
